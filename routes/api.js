b24b017d92bfa5b172ab2670f6bb3c0c1ec20e71
Chris Williams <cwill833@gmail.com>
2019-07-08 10:34:10 -0500

    first commit

---
+++ routes/api.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var blogCtrl = require('../controllers/blogs');
+
+/* GET /api/puppies */
+router.get('/posts', blogCtrl.getAllPosts);
+router.get('/post/:id', blogCtrl.getOnePost);
+router.post('/post', blogCtrl.createPost);
+router.delete('/post/:id', blogCtrl.deletePost);
+
+module.exports = router;
