7632e993ea10cc3e3156b0074818563d9540130e
Chris Williams <cwill833@gmail.com>
2019-07-09 22:27:51 -0500

    add api routs

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var blogCtrl = require('../controllers/blogs');
 
-/* GET /api/puppies */
+/* GET /api/posts*/
 router.get('/posts', blogCtrl.getAllPosts);
 router.get('/post/:id', blogCtrl.getOnePost);
 router.post('/post', blogCtrl.createPost);
