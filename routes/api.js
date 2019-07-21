36ebd713a28ced96d170ed9e483849ce1c14c350
Murray918 <darbyguitars@gmail.com>
2019-07-21 16:38:36 -0500

    added the update functionalit and also added catches to all of the controllers

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,11 @@
-var express = require('express');
-var router = express.Router();
-var blogCtrl = require('../controllers/blogs');
+var express = require('express')
+var router = express.Router()
+var blogCtrl = require('../controllers/blogs')
 
 /* GET /api/puppies */
-router.get('/posts', blogCtrl.getAllPosts);
-router.get('/post/:id', blogCtrl.getOnePost);
-router.post('/post', blogCtrl.createPost);
-router.delete('/post/:id', blogCtrl.deletePost);
+router.get('/posts', blogCtrl.getAllPosts)
+router.put('/post/:id', blogCtrl.getOnePost)
+router.post('/post', blogCtrl.createPost)
+router.delete('/post/:id', blogCtrl.deletePost)
 
-module.exports = router;
+module.exports = router
